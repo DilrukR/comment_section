d0807a1e2ff575db55466b76e8bb6e4e16a9d91c
DilrukR <68820399+DilrukR@users.noreply.github.com>
2021-06-29 00:51:46 +0530

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//mobile menu//
+const burgerIcon = document.querySelector('#burger');
+const navbarmenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click',() =>{
+
+  navbarmenu.classList.toggle('is-active')
+
+})
+
+// mobile-dropdown-navbar
